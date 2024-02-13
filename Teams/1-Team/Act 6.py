d5622587d6cd397e80028e9be9e30b653e3b9738
fergalindo-10 <157067407+fergalindo-10@users.noreply.github.com>
2024-02-13 15:23:29 -0600

    Add files via upload

---
+++ Teams/1-Team/Act 6.py
@@ -0,0 +1,10 @@
+class Restaurante: 
+platillos=["Arroz chino","Sushi frio","Sushi empanizado"]
+año_de_fundacion=1992
+
+    def_init_(self, nombre, tipo_restaurante,fundacion,nombre_mascota)
+
+self.identidad= nombre
+self.tipo= tipo_de_restaurante
+self.fundacion 
+
