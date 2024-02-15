68c70fadbb436b9e21bbac9f73df8fbd99d562ad
PRISCILAGARCIA18 <157067219+PRISCILAGARCIA18@users.noreply.github.com>
2024-02-15 14:44:37 -0600

    Update #trabajo.py

--- Teams/3-Team/#trabajo.py
+++ Teams/3-Team/#trabajo.py
@@ -1,2 +1,11 @@
 #trabajo
-nombrerestaurante=lascoquettes
+
+# dedinicion de clase
+class Restaurante: 
+  platillos= ["tacos trompo" , "tacos bistec", "campechanas" , "gringas",  "hamburgesas" , "frijoles" ,"burrito ", "tacos de carne asado", "tacos de suadero", "papa asada" ]
+bebidas=  
+  año_de_fundacion = 1888
+  nombre_de_la_mascota = Dontaquito
+# inicializacion
+def_int_ (self,nombre ,tipo_de_restaurante,fundacion):
+
