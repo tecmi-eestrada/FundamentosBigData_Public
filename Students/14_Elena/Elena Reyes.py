8caed724edfbd44bfe5f9fe02e8a8f0e3d3944b0
ElenaRey86 <157067124+ElenaRey86@users.noreply.github.com>
2024-02-27 14:55:16 -0600

    Add files via upload

---
+++ Students/14_Elena/Elena Reyes.py
@@ -0,0 +1,14 @@
+class ElenaReyes:
+    Segundo_nombre= "Camila"
+    Segundo_apellido="Valles"
+    Color_favorito ="Mi color favorito es el negro, "
+    hobie="mi hobie es escuchar musica, "
+    Deporte="mi deporte favorito es el tenis de mesa."
+    Musica="La musica que más he escuchado es del grupo de Cuarteto de Nos, "
+    Cancion="y la cancion que más me gusta de ellos es Tiburones en el bosque"
+
+
+Mi_primer_objeto=ElenaReyes()
+print("Mis segundo nombre y mi segundo apellido son : " + Mi_primer_objeto.Segundo_nombre, Mi_primer_objeto.Segundo_apellido)
+print(Mi_primer_objeto.Color_favorito,Mi_primer_objeto.hobie,Mi_primer_objeto.Deporte)
+print(Mi_primer_objeto.Musica,Mi_primer_objeto.Cancion)
